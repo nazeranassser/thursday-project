015e4e998727342e9b23a06edb20873f636e9f30
nazeranassser <159236095+nazeranassser@users.noreply.github.com>
2024-02-15 13:53:02 +0300

    Add files via upload

---
+++ thursdayprojectjs.js
@@ -0,0 +1,21 @@
+function onchangeemail() {
+    var emailValue = document.getElementById("email").value;
+    alert("Email: " + emailValue);
+  }
+  
+  function onchangepassword() {
+    var passwordValue = document.getElementById("password").value;
+    alert("Password: " + passwordValue);
+  }
+  
+  function onchangecontact() {
+    var contactValue = document.getElementById("contact").value;
+    alert("Contact Number: " + contactValue);
+  }
+  function onchangecountry() {
+    var countryValue = document.getElementById("country").value;
+    alert("country: " + countryValue);
+  }
+  
+  function onclicksubmit() {
+    alert("Form submitted");}
